c021f9ac1afccef797e5ca689bbc04f8c3d39a06
Ricardo <rjcjunior@hotmail.com>
2020-07-14 09:03:29 -0300

    Updates

---
+++ precache-manifest.a671fcacc195430f7e16241ba47dcf06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74322aa6324169b980a52bd8e41e6716",
+    "url": "/reddit-reactJs/index.html"
+  },
+  {
+    "revision": "b2f9e5882db96797fed3",
+    "url": "/reddit-reactJs/static/js/2.4e4cedfd.chunk.js"
+  },
+  {
+    "revision": "0e3d1f32e6a7a27a748a3d56dde7ac82",
+    "url": "/reddit-reactJs/static/js/2.4e4cedfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e54f5d1ddb3fa66437",
+    "url": "/reddit-reactJs/static/js/main.6d74a9cd.chunk.js"
+  },
+  {
+    "revision": "46b768b544cafaa092d7",
+    "url": "/reddit-reactJs/static/js/runtime-main.0c029412.js"
+  }
+]);
